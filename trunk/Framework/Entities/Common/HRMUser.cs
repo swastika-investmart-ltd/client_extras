c93b5510d5370f70d9657074ef86a0bb62033a00
Mahesh Rathore <mahesh.rathore@swastika.co.in>
2024-09-10 15:36:26 +0530

    commit hrmsUser

--- trunk/Framework/Entities/Common/HRMUser.cs
+++ trunk/Framework/Entities/Common/HRMUser.cs
@@ -11,13 +11,12 @@ namespace Entities.Common
         public string EmployeeEmail { get; set; }
         public string EmployeeCode { get; set; }
         public string EmployeeUserName { get; set; }
-
         public long BranchId { get; set; }
         public string BranchName { get; set; }
         public string BranchCode { get; set; }
-
         public long ProfileId { get; set; }
         public string ProfileName { get; set; }
         public string Extension { get; set; }
+
     }
 }
