31dcaaea1dbace96c42b93ed052070097c8c174e
pawanpatidarswastika <pawan.patidar@swastika.co.in>
2025-07-14 17:13:08 +0530

    Bulk contact insert in jarvis.

---
+++ trunk/Services/Client.WebApi/Models/Contacts/ContactsReq.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Client.WebApi.Models
+{
+    public class InsertContactReq
+    {
+        [Required]
+        public List<Contacts> ContactJSON { get; set; }
+    }
+    public class Contacts
+    {
+        public string ClientID { get; set; }
+        public string Name { get; set; }
+        public string MobileNo { get; set; }
+    }
+}
